eef2ba3e543df230290627b8c1828e51770bdf91
Bartek <bartlomiej.pawel.borzyszkowski@intel.com>
2020-07-22 13:32:21 +0200

    Code refacored and bumped requirements

--- ANN-Autoencoder/eval.py
+++ ANN-Autoencoder/eval.py
@@ -1,37 +1,27 @@
-# import libraries
+""" Evaluation of models trained for anomaly detection. """
+
 import os
-import requests
+import argparse
 import pandas as pd
 import numpy as np
-import setGPU
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.externals import joblib
 import seaborn as sns
-sns.set(color_codes=True)
 import matplotlib.pyplot as plt
-#%matplotlib inline
 import h5py as h5
+from sklearn.externals import joblib
 from gwpy.timeseries import TimeSeries
-
-from numpy.random import seed
-from tensorflow import set_random_seed
-import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.ERROR)
-
-from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector
-from keras.models import Model
-from keras import regularizers
 from keras.models import load_model
 from random import sample as RandSample
-import argparse
 
-from model import autoencoder_LSTM, autoencoder_Conv, autoencoder_DeepConv
+sns.set(color_codes=True)
+
 
 def filters(array, sample_frequency):
+    """ Apply preprocessing such as whitening and bandpass """
     strain = TimeSeries(array, sample_rate=int(sample_frequency))
-    white_data = strain.whiten(fftlength=4,fduration=4)
+    white_data = strain.whiten(fftlength=4, fduration=4)
     bp_data = white_data.bandpass(50, 250)
-    return(bp_data.value)
+    return bp_data.value
+
 
 def main(args):
     outdir = args.outdir
@@ -39,17 +29,16 @@ def main(args):
     freq = args.freq
     filtered = args.filtered
     timesteps = int(args.timesteps)
-    os.system('mkdir -p %s'%outdir)
-    
-    load = h5.File('data/default.hdf','r')
-    
+    os.system('mkdir -p %s' % outdir)
+
+    load = h5.File('../../dataset/default.hdf', 'r')
+
     ''' #### WONT WORRY ABOUT THRESHOLDING FOR NOW
     noise_samples = load['noise_samples']['%s_strain'%(str(detector).lower())][:][:10]
     
     # Definining frequency in Hz instead of KHz
     #freq = 1000*int(freq)
     freq = 4000
-    
     
     if bool(int(filtered)):
         print('filtering data with whitening and bandpass')
@@ -72,7 +61,6 @@ def main(args):
     # load the autoencoder network model
     model = load_model('%s/best_model.hdf5'%(outdir))
     
-    
     ### Evaluating on training data to find threshold ### 
     print('Evaluating Model on train data. This make take a while...')
     X_pred = model.predict(X_train)
@@ -91,85 +79,94 @@ def main(args):
     threshold = scored.max()[0]
     print('The threshold is: %s'%(str(threshold)))
     '''
-    
-    if int(freq) == 2: 
+
+    # Define frequency in Hz instead of KHz
+    if int(freq) == 2:
         freq = 2048
-    elif int(freq) == 4: 
+    elif int(freq) == 4:
         freq = 4096
-        
-    # Evaluate on 10 test data events 
-    injection_samples = load['injection_samples']['%s_strain'%(str(detector).lower())][:][:1000]
+    else:
+        return print(f'Given frequency {freq} is not supported')
+
+    # Evaluate model on 10 test data events
+    injection_samples = load['injection_samples']['%s_strain' % (str(detector).lower())][:][:1000]
     times = load['injection_samples']['event_time']
     random_samples = RandSample(range(0, len(injection_samples)), 10)
-    
+
     if bool(int(filtered)):
-        print('filtering data with whitening and bandpass')
+        print('Filtering data with whitening and bandpass')
+        print(f'Sample Frequency: {freq} Hz')
         x = [filters(sample, freq) for sample in injection_samples]
-        print('Done!')
-        
+        print('Filtering completed')
+
     # Normalize the data
-    scaler_filename = "%s/scaler_data_%s"%(outdir, detector)
-    scaler = joblib.load(scaler_filename) 
+    scaler_filename = f"{outdir}/scaler_data_{detector}"
+    scaler = joblib.load(scaler_filename)
     X_test = scaler.transform(x)
-    #X_test = scaler.transform(y.reshape(-1, 1))
-    
-    for random_sample in random_samples: 
+    # X_test = scaler.transform(y.reshape(-1, 1))
+
+    for random_sample in random_samples:
         event = X_test[random_sample]
         time = times[random_sample] - 1000000000
-        
-        if event.shape[0]%timesteps != 0: 
-            event = event[:-1*int(event.shape[0]%timesteps)]
+
+        if event.shape[0] % timesteps != 0:
+            event = event[:-1 * int(event.shape[0] % timesteps)]
         event = event.reshape(-1, timesteps, 1)
-        
+
         # load the autoencoder network model
-        model = load_model('%s/best_model.hdf5'%(outdir))
-        
+        model = load_model(f'{outdir}/best_model.hdf5')
+
         batch_loss = []
-        for batch in event: 
+        for batch in event:
             batch_loss.append(model.evaluate(batch.reshape(1, timesteps, 1), batch.reshape(1, timesteps, 1), verbose=0))
-            
+
         fig, ax = plt.subplots(figsize=(14, 6), dpi=80)
         ax.plot(batch_loss)
-        plt.axvline(len(batch_loss)*5.5/8, label='actual GW event', color='green')
-        plt.savefig('%s/batchloss_%s.jpg'%(outdir,time))
-        
+        plt.axvline(len(batch_loss) * 5.5 / 8, label='actual GW event', color='green')
+        plt.savefig(f'{outdir}/batchloss_{time}.jpg')
+
         X_pred_test = np.array(model.predict(event))
-        
+
         fig, ax = plt.subplots(figsize=(14, 6), dpi=80)
-        ax.plot(event.reshape(-1)[int(2048*5.5) - 300:int(2048*5.5) + 300], label='truth')
-        ax.plot(X_pred_test.reshape(-1)[int(2048*5.5)- 300:int(2048*5.5) + 300], label='predict')
+        ax.plot(event.reshape(-1)[int(2048 * 5.5) - 300:int(2048 * 5.5) + 300], label='truth')
+        ax.plot(X_pred_test.reshape(-1)[int(2048 * 5.5) - 300:int(2048 * 5.5) + 300], label='predict')
         plt.legend(loc='upper left')
         plt.title('LSTM Autoencoder')
-        plt.savefig('%s/middle30ms_%s.jpg'%(outdir,time))
-        
+        plt.savefig(f'{outdir}/middle30ms_{time}.jpg')
+
         print(X_pred_test.shape)
-        X_pred_test = X_pred_test.reshape(X_pred_test.shape[0]*timesteps, X_pred_test.shape[2])
-        
-        #X_pred_train.index = train.index
-        Xtest = event.reshape(event.shape[0]*timesteps, event.shape[2])
+        X_pred_test = X_pred_test.reshape(X_pred_test.shape[0] * timesteps, X_pred_test.shape[2])
+
+        # X_pred_train.index = train.index
+        Xtest = event.reshape(event.shape[0] * timesteps, event.shape[2])
 
         X_pred_test = pd.DataFrame(X_pred_test)
         scored_test = pd.DataFrame()
-        scored_test['Loss_mae'] = np.mean(np.abs(X_pred_test-Xtest), axis = 1)
-        #scored_test['Threshold'] = threshold
-        #scored_test['Anomaly'] = scored_test['Loss_mae'] > scored_test['Threshold']
-        #scored_test.plot(logy=True,  figsize=(16,9), ylim=[t/(1e2),threshold*(1e2)], color=['blue','red'])
-        scored_test.plot(logy=False,  figsize=(16,9), color=['blue','red'])
-        plt.axvline(5.5*2048, label='actual GW event', color='green') #Sampling rate of 2048 Hz with the event occuring 5.5 seconds into sample
+        scored_test['Loss_mae'] = np.mean(np.abs(X_pred_test - Xtest), axis=1)
+        # scored_test['Threshold'] = threshold
+        # scored_test['Anomaly'] = scored_test['Loss_mae'] > scored_test['Threshold']
+        # scored_test.plot(logy=True,  figsize=(16,9), ylim=[t/(1e2),threshold*(1e2)], color=['blue','red'])
+        scored_test.plot(logy=False, figsize=(16, 9), color=['blue', 'red'])
+        # Sampling rate of 2048 Hz with the event occuring 5.5 seconds into sample
+        plt.axvline(5.5 * 2048, label='actual GW event', color='green')
         plt.legend(loc='upper left')
-        plt.savefig('%s/test_threshold_%s_8sec.jpg'%(outdir, time))
-        
-    
+        plt.savefig(f'{outdir}/test_threshold_{time}_8sec.jpg')
+
+
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
-    
+
     # Required positional arguments
     parser.add_argument("outdir", help="Required output directory")
-    parser.add_argument("detector", help="Required output directory")
-    parser.add_argument("--freq", help="Sampling frequency of detector in KHz", action='store', dest='freq', default = 4)
-    parser.add_argument("--filtered", help="Apply LIGO's bandpass and whitening filters", action='store', dest='filtered', default = 1)
-    parser.add_argument("--timesteps", help="Number of timesteps passed to LSTM", action='store', dest='timesteps', default = 100)
-    
+    parser.add_argument("detector", help="LIGO Detector")
+
+    # Additional arguments
+    parser.add_argument("--freq", help="Sampling frequency of detector in KHz",
+                        action='store', dest='freq', default=2)
+    parser.add_argument("--filtered", help="Apply LIGO's bandpass and whitening filters",
+                        action='store', dest='filtered', default=1)
+    parser.add_argument("--timesteps", help="Number of timesteps passed to LSTM",
+                        action='store', dest='timesteps', default=100)
+
     args = parser.parse_args()
     main(args)
---
+++ ANN-Autoencoder/model.py
@@ -0,0 +1,133 @@
+""" Set of autoencoders models used for anomaly detection. """
+
+from keras.layers import Input, Dense, LSTM, TimeDistributed, RepeatVector, Conv1D, \
+    MaxPooling1D, UpSampling1D, Flatten, Reshape
+from keras.models import Model
+from keras import regularizers
+
+
+def autoencoder_LSTM(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = LSTM(32, activation='relu', return_sequences=True,
+              kernel_regularizer=regularizers.l2(0.00))(inputs)
+    L2 = LSTM(8, activation='relu', return_sequences=False)(L1)
+    L3 = RepeatVector(X.shape[1])(L2)
+    L4 = LSTM(8, activation='relu', return_sequences=True)(L3)
+    L5 = LSTM(32, activation='relu', return_sequences=True)(L4)
+    output = TimeDistributed(Dense(X.shape[2]))(L5)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_Conv(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(4, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(encoded)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(16, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(4)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_ConvDNN(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(32, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(2, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    x = Flatten()(encoded)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(20, activation='relu')(x)
+    x = Dense(10, activation='relu')(x)
+    x = Dense(20, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(130, activation='relu')(x)
+    x = Reshape((13, 10))(x)
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(x)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(32, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(2)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_ConvLSTM(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(4, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    x = Reshape((130, 1))(encoded)
+
+    x = LSTM(32, activation='relu', return_sequences=False,
+             kernel_regularizer=regularizers.l2(0.00))(x)
+    x = RepeatVector(130)(x)
+    x = LSTM(32, activation='relu', return_sequences=True)(x)
+    out = TimeDistributed(Dense(1))(x)
+
+    x = Reshape((13, 10))(out)
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(x)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(32, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(4)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_DeepConv(X):
+    ### Use autoencoder_ConvDNN instead ###
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    x = Conv1D(16, 16, activation="relu", padding="same")(inputs)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Conv1D(32, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Conv1D(64, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Flatten()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(10, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(64, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(32, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(16, 16, activation="relu", padding="same")(inputs)
+    x = Dense(X.shape[1], activation='relu')(x)
+    output = Reshape((X.shape[1], 1))(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_DNN(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    x = Flatten()(inputs)
+    x = Dense(int(X.shape[1] / 2), activation='relu')(x)
+    x = Dense(int(X.shape[1] / 10), activation='relu')(x)
+    x = Dense(int(X.shape[1] / 2), activation='relu')(x)
+    x = Dense(X.shape[1], activation='relu')(x)
+    output = Reshape((X.shape[1], 1))(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
