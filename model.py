4d389c1b7d8af69178fc12d3adc177dd6ddbc1a9
eric-moreno <emoreno@caltech.edu>
2020-07-23 01:28:42 -0700

    added gru autoencoder

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from tensorflow import set_random_seed
 import tensorflow as tf
 #tf.logging.set_verbosity(tf.logging.ERROR) 
 
-from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, Flatten, Reshape
+from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, Flatten, Reshape, GRU
 from keras.models import Model
 from keras import regularizers, Sequential 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -17,6 +17,18 @@ def autoencoder_LSTM(X):
     L3 = RepeatVector(X.shape[1])(L2)
     L4 = LSTM(8, activation='relu', return_sequences=True)(L3)
     L5 = LSTM(32, activation='relu', return_sequences=True)(L4)
+    output = TimeDistributed(Dense(X.shape[2]))(L5)    
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+def autoencoder_GRU(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = GRU(32, activation='relu', return_sequences=True, 
+              kernel_regularizer=regularizers.l2(0.00))(inputs)
+    L2 = GRU(8, activation='relu', return_sequences=False)(L1)
+    L3 = RepeatVector(X.shape[1])(L2)
+    L4 = GRU(8, activation='relu', return_sequences=True)(L3)
+    L5 = GRU(32, activation='relu', return_sequences=True)(L4)
     output = TimeDistributed(Dense(X.shape[2]))(L5)    
     model = Model(inputs=inputs, outputs=output)
     return model
@@ -42,27 +54,25 @@ def autoencoder_Conv(X):
 
 def autoencoder_ConvDNN(X):
     inputs = Input(shape=(X.shape[1], X.shape[2]))
-    L1 = Conv1D(32, 3, activation="relu", padding="same")(inputs) # 10 dims
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs) # 10 dims
     #x = BatchNormalization()(x)
-    L2 = MaxPooling1D(2, padding="same")(L1) # 5 dims
+    L2 = MaxPooling1D(4, padding="same")(L1) # 5 dims
     L3 = Conv1D(10, 3, activation="relu", padding="same")(L2) # 5 dims
     #x = BatchNormalization()(x)
     encoded = MaxPooling1D(4, padding="same")(L3) # 3 dims
     x = Flatten()(encoded)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(20, activation='relu')(x)
+    x = Dense(30, activation='relu')(x)
     x = Dense(10, activation='relu')(x)
-    x = Dense(20, activation='relu')(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(130, activation='relu')(x)
-    x = Reshape((13, 10))(x)
+    x = Dense(30, activation='relu')(x)
+    x = Dense(70, activation='relu')(x)
+    x = Reshape((7, 10))(x)
     # 3 dimensions in the encoded layer
     L4 = Conv1D(10, 3, activation="relu", padding="same")(x) # 3 dims
     #x = BatchNormalization()(x)
     L5 = UpSampling1D(4)(L4) # 6 dims
-    L6 = Conv1D(32, 2, activation='relu')(L5) # 5 dims
+    L6 = Conv1D(16, 2, activation='relu')(L5) # 5 dims
     #x = BatchNormalization()(x)
-    L7 = UpSampling1D(2)(L6) # 10 dims
+    L7 = UpSampling1D(4)(L6) # 10 dims
     output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7) # 10 dims
     model = Model(inputs=inputs, outputs = output)
     return model 
