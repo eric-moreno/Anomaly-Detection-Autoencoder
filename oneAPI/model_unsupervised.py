4da22a8434ff6e5f4db9060a058c1c4bae704182
Bartek <bartlomiej.pawel.borzyszkowski@intel.com>
2020-09-03 17:55:40 +0200

    unsupervised learning in hls4ml with oneAPI

---
+++ oneAPI/model_unsupervised.py
@@ -0,0 +1,167 @@
+""" Set of autoencoders models used for anomaly detection. """
+
+from keras.layers import Input, Dense, LSTM, TimeDistributed, RepeatVector, Conv1D, \
+    MaxPooling1D, UpSampling1D, Flatten, Reshape, GRU
+from keras.models import Model
+from keras import regularizers
+
+
+def autoencoder_LSTM(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = LSTM(32, activation='relu', return_sequences=True,
+              kernel_regularizer=regularizers.l2(0.00))(inputs)
+    L2 = LSTM(8, activation='relu', return_sequences=False)(L1)
+    L3 = RepeatVector(X.shape[1])(L2)
+    L4 = LSTM(8, activation='relu', return_sequences=True)(L3)
+    L5 = LSTM(32, activation='relu', return_sequences=True)(L4)
+    output = TimeDistributed(Dense(X.shape[2]))(L5)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_GRU(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = GRU(32, activation='relu', return_sequences=True,
+             kernel_regularizer=regularizers.l2(0.00))(inputs)
+    L2 = GRU(8, activation='relu', return_sequences=False)(L1)
+    L3 = RepeatVector(X.shape[1])(L2)
+    L4 = GRU(8, activation='relu', return_sequences=True)(L3)
+    L5 = GRU(32, activation='relu', return_sequences=True)(L4)
+    output = TimeDistributed(Dense(X.shape[2]))(L5)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_Conv(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(4, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(encoded)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(16, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(4)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_Conv2(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 4, activation="relu", dilation_rate=1, padding="same")(inputs)
+    L2 = MaxPooling1D(2)(L1)
+    L3 = Conv1D(32, 4, activation="relu", dilation_rate=2, padding="same")(L2)
+    L4 = MaxPooling1D(2)(L3)
+    L5 = Conv1D(64, 4, activation="relu", dilation_rate=2, padding="same")(L4)
+    L6 = MaxPooling1D(4)(L5)
+    L7 = Conv1D(128, 8, activation="relu", dilation_rate=2, padding="same")(L6)
+    encoded = MaxPooling1D(4)(L7)
+    L7 = Conv1D(128, 8, activation="relu", dilation_rate=2, padding="same")(encoded)
+    L8 = UpSampling1D(4)(L7)
+    L9 = Conv1D(64, 4, activation="relu", dilation_rate=2, padding="same")(L8)
+    L10 = UpSampling1D(4)(L9)
+    L11 = Conv1D(32, 4, activation="relu", dilation_rate=2, padding="same")(L10)
+    L12 = UpSampling1D(4)(L11)
+    L13 = Conv1D(16, 3, activation="relu", dilation_rate=1, padding="same")(L12)
+    L14 = UpSampling1D(2)(L13)
+    output = Conv1D(1, 4, activation="relu", dilation_rate=1, padding="same")(L12)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_ConvDNN(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(4, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    x = Flatten()(encoded)
+    x = Dense(30, activation='relu')(x)
+    x = Dense(10, activation='relu')(x)
+    x = Dense(30, activation='relu')(x)
+    x = Dense(70, activation='relu')(x)
+    x = Reshape((7, 10))(x)
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(x)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(16, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(4)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_ConvLSTM(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 3, activation="relu", padding="same")(inputs)  # 10 dims
+    # x = BatchNormalization()(x)
+    L2 = MaxPooling1D(4, padding="same")(L1)  # 5 dims
+    L3 = Conv1D(10, 3, activation="relu", padding="same")(L2)  # 5 dims
+    # x = BatchNormalization()(x)
+    encoded = MaxPooling1D(4, padding="same")(L3)  # 3 dims
+    x = Reshape((70, 1))(encoded)
+
+    x = LSTM(32, activation='relu', return_sequences=False,
+             kernel_regularizer=regularizers.l2(0.00))(x)
+    x = RepeatVector(70)(x)
+    x = LSTM(32, activation='relu', return_sequences=True)(x)
+    out = TimeDistributed(Dense(1))(x)
+
+    x = Reshape((7, 10))(out)
+    # 3 dimensions in the encoded layer
+    L4 = Conv1D(10, 3, activation="relu", padding="same")(x)  # 3 dims
+    # x = BatchNormalization()(x)
+    L5 = UpSampling1D(4)(L4)  # 6 dims
+    L6 = Conv1D(32, 2, activation='relu')(L5)  # 5 dims
+    # x = BatchNormalization()(x)
+    L7 = UpSampling1D(4)(L6)  # 10 dims
+    output = Conv1D(1, 3, activation='sigmoid', padding='same')(L7)  # 10 dims
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_DeepConv(X):
+    ### Use autoencoder_ConvDNN instead ###
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    x = Conv1D(16, 16, activation="relu", padding="same")(inputs)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Conv1D(32, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Conv1D(64, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = MaxPooling1D(4, padding="same")(x)
+    x = Flatten()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(10, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(64, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(32, 8, activation="relu", padding="same", dilation_rate=4)(x)
+    x = UpSampling1D(4)(x)
+    x = Conv1D(16, 16, activation="relu", padding="same")(x)
+    x = Dense(X.shape[1], activation='relu')(x)
+    output = Reshape((X.shape[1], 1))(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def autoencoder_DNN(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    x = Flatten()(inputs)
+    x = Dense(int(X.shape[1] / 2), activation='relu')(x)
+    x = Dense(int(X.shape[1] / 10), activation='relu')(x)
+    x = Dense(int(X.shape[1] / 2), activation='relu')(x)
+    x = Dense(X.shape[1], activation='relu')(x)
+    output = Reshape((X.shape[1], 1))(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
---
+++ oneAPI/train_unsupervised.py
@@ -0,0 +1,68 @@
+""" Train autoencoder for anomaly detection in given time series data. """
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py as h5
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from model_unsupervised import autoencoder_LSTM, autoencoder_ConvLSTM, autoencoder_ConvDNN, autoencoder_DNN, \
+    autoencoder_Conv, autoencoder_Conv2
+
+
+def hls4ml_deployment(model, test_data, test_truth):
+    import hls4ml
+    config = hls4ml.utils.config_from_keras_model(model, granularity='model', default_precision='f32')
+    print(config)
+    hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config, backend='oneAPI', device="cpu",
+                                                           output_dir='test')
+    hls_model.compile(batch_size=1)
+    y_hls = hls_model.predict(test_data.astype(np.float32))
+    # print(f"Y hls: {y_hls}")
+    from sklearn.metrics import accuracy_score
+    print("Keras  Accuracy: {}".format(
+        accuracy_score(np.argmax(test_truth, axis=1), np.argmax(model.predict(test_data), axis=1))))
+    print("hls4ml Accuracy: {}".format(accuracy_score(np.argmax(test_truth, axis=1), np.argmax(y_hls, axis=1))))
+
+
+def main():
+    """ Main function to prepare and train the model """
+    outdir = 'oneAPI-model'
+    os.system('mkdir -p %s' % outdir)
+
+    load = h5.File('data.h5', 'r')
+
+    datapoints = 1200
+    X_train = load['data'][:]
+
+    # Reshape inputs for LSTM [samples, timesteps, features]
+    X_train = X_train.reshape(-1, datapoints, 1)
+    print("Training data shape:", X_train.shape)
+
+    # Define the model
+    model = autoencoder_DNN(X_train)
+    model.compile(optimizer='adam', loss='mse')
+    model.summary()
+
+    # Fit the model to the data
+    nb_epochs = 1
+    batch_size = 1024
+    early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+    mcp_save = ModelCheckpoint(f'{outdir}/best_model.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+    history = model.fit(X_train, X_train, epochs=nb_epochs, batch_size=batch_size,
+                        validation_split=0.2, callbacks=[early_stop, mcp_save]).history
+    model.save(f'{outdir}/last_model.hdf5')
+
+    fig, ax = plt.subplots(figsize=(14, 6), dpi=80)
+    ax.plot(history['loss'], 'b', label='Train', linewidth=2)
+    ax.plot(history['val_loss'], 'r', label='Validation', linewidth=2)
+    ax.set_title('Model loss', fontsize=16)
+    ax.set_ylabel('Loss (mse)')
+    ax.set_xlabel('Epoch')
+    ax.legend(loc='upper right')
+    plt.savefig(f'{outdir}/loss.png')
+
+    hls4ml_deployment(model, X_train, X_train)
+
+
+if __name__ == "__main__":
+    main()
